870c51f19dc17e416aa5655bb2e735ce0f40e202
Jonathan K. <noudas1@gmail.com>
2024-12-18 17:20:14 -0300

    Add Enemy Class

--- classes/enemyClass.js
+++ classes/enemyClass.js
@@ -1,1 +1,43 @@
 const Entity = require("./entityClass")
+
+class Enemy extends Entity {
+    constructor(health = 10, attack = 10, weakness = "") {
+        super(health, attack);
+        this.weakness = weakness;
+    }
+
+        isWeakTo(attackType) {
+            return attackType.toLowerCase() === this.weakness.toLowerCase();
+        }
+    
+        takeDamage(amount, attackType = "") {
+            if (this.isWeakTo(attackType)) {
+                amount *= 2;
+            }
+            super.takeDamage(amount);
+        }
+    
+        attackPlayer(player) {
+            player.takeDamage(this.attack);
+            if (player.health <= 0) {
+                this.win_condition = true;
+            }
+        }
+    
+        checkDeathCondition() {
+            if (this.health <= 0) {
+                this.lose_condition = true;
+            }
+            return this.lose_condition;
+        }
+    
+        checkWinCondition(player) {
+            if (player.health <= 0) {
+                this.win_condition = true;
+            }
+            return this.win_condition;
+        }
+    }
+
+
+}
