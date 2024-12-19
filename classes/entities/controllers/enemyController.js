b628d0597540e941566d1b718e93d665b0c74834
Jonathan K. <noudas1@gmail.com>
2024-12-19 08:43:18 -0300

    EnemyController find enemy

--- classes/entities/controllers/enemyController.js
+++ classes/entities/controllers/enemyController.js
@@ -1,1 +1,25 @@
 const Enemy = require("../config/enemyClass")
+
+class EnemyController{
+    constructor(){
+        this.enemies = {}
+    };
+
+    createEnemy(name, health = 10, attack = 10, weakness = "") {
+        if (this.enemies[name]) {
+            throw new Error("Enemy with this name already exists.");
+        }
+        const enemy = new Enemy(health, attack, weakness);
+        this.enemies[name] = enemy;
+        return enemy;
+    };
+
+    findEnemyByName(name) {
+        const enemy = this.enemies[name];
+        if (!enemy) {
+            throw new Error("Enemy not found.");
+        }
+        return enemy;
+    };
+
+};
