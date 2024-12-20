b6a5400a66dc7e9da1d2abc595886860efabcb24
GBlecher <gilanblecher@gmail.com>
2024-12-20 15:21:43 +0200

    initialise enemy cards

--- controllers/enemyController.js
+++ controllers/enemyController.js
@@ -63,8 +63,8 @@ class EnemyController {
     }
 
     getAllEnemies() {
-        return Object.values(this.enemies);
-    }
+        return Object.entries(this.enemies).map(([name, attributes]) => ({ name, attributes }));
+    }
 
     removeEnemy(name) {
         const enemy = this.findEnemyByName(name);
