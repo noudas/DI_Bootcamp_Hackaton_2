6e4b6cf8075ae157f8dc80f00268cafab8a46bd1
Jonathan K. <noudas1@gmail.com>
2024-12-22 10:04:25 -0300

    undoing

--- server.js
+++ server.js
@@ -1,23 +1,21 @@
-import express from "express";
-import { createEnemiesTable, db } from './dbConnection/database.js';
-import cors from "cors";
-import { catRouter } from './routes/catRouter.js';
-import playerRoutes from "./routes/playerRoute.js";
-import enemyRoutes from "./routes/enemyRoute.js";
-import path from "path";
-import bp from 'body-parser'
+const express = require("express");
+
+const cors = require("cors");
+const {catRouter}= require('./routes/catRouter.js');
+const playerRoutes = require("./routes/playerRoute");
+const enemyRoutes = require("./routes/enemyRoute");
 
 const PORT = 5000;
 
 const app = express();
 app.use(cors());
-app.use("/", express.static(path.join(path.resolve(), "public")));
-// app.use(express.json());
-app.use(bp.json());
-app.use(bp.urlencoded({extended:false}));
+app.use("/", express.static(__dirname + "/public"));
+app.use(express.json());
+
 app.use("/players", playerRoutes);
 app.use("/enemies", enemyRoutes);
 app.use("/categories",catRouter)
+
 
 
 // Health Check Endpoint
@@ -34,3 +32,4 @@ app.use((err, req, res, next) => {
 app.listen(PORT, () => {
   console.log(`run on ${PORT}`);
 });
+
